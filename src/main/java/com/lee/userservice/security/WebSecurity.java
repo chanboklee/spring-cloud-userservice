cd3670d3b17a6cba7483878ce94978e730aac688
chanboklee <chanboklee@naver.com>
2023-01-04 22:33:27 +0900

    Fix: Spring-Cloud-Userservice - add actuator..

--- src/main/java/com/lee/userservice/security/WebSecurity.java
+++ src/main/java/com/lee/userservice/security/WebSecurity.java
@@ -26,6 +26,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
                         .hasIpAddress("192.168.45.42")
                         .and()
